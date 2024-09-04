f4e04fcc5eec2d71c319c729e7bc006bf2e8cdd0
LUIS\jl_vi <jl_vidaloc@hotmail.com>
2024-09-04 12:42:52 -0600

    Se concluye el microservicio de choferes

---
+++ Gen9-Parent/Gen9-Negocio/Gen9-Choferes/src/main/java/com/vidal/choferes/dao/IChoferesDao.java
@@ -0,0 +1,10 @@
+package com.vidal.choferes.dao;
+
+
+import com.vidal.choferes.models.Chofer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IChoferesDao extends JpaRepository<Chofer,Long> {
+
+}
+ 
